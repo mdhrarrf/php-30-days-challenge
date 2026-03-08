e4558967a147aa57d38fb5554cde081995d74866
mdhrarrf <haidarmeeteng@gmail.com>
2026-03-08 17:45:59 +0700

    arrays

---
+++ 01-logic-foundations/day-04-arrays/01-biodata.php
@@ -0,0 +1,10 @@
+<?php
+
+$biodata = [
+    "nama" => "Muhammad Haidar Almer Rafif",
+    "umur" => "16 Tahun",
+    "hobi" => "Ngoding",
+    "cita_cita" => "Programmer"
+];
+
+echo "Halo, saya {$biodata['nama']}. Saya berumur {$biodata['umur']} tahun dan ingin menjadi seorang {$biodata['cita_cita']}.";
